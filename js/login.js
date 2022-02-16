d7b13d0e67558300345d5ed5c20cc65402406386
chanhi <61691088+chanhi@users.noreply.github.com>
2022-02-16 23:23:02 +0900

    update Reminder!
    complete to making login.js

--- js/login.js
+++ js/login.js
@@ -1,18 +1,20 @@
-//const bodyHome = document.querySelector(".home");
+const sidebarNav = document.querySelector(".sidebar");
 const loginForm = document.querySelector("#login-form");
 const logoutForm = document.querySelector("#logout-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
-const loginTodo = document.getElementById("todo-form");
 const alertLogin = document.getElementById("alert-login");
+const sidebarLogout = document.querySelector("#sidebar-logout");
 
 const HIDDEN_CLASSNAME = "hidden";
+const NONE_CLICK = "none-click";
 const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault(); // submit 의 특성상 새로고침을 하는데 이것을 막음
     loginForm.classList.add(HIDDEN_CLASSNAME);
     alertLogin.classList.add(HIDDEN_CLASSNAME);
+    sidebarNav.classList.remove(NONE_CLICK)
     const username = loginInput.value;
     localStorage.setItem(USERNAME_KEY, username);
     paintGreetings(username);
@@ -27,7 +29,6 @@ function paintGreetings(username) {
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
     logoutForm.classList.remove(HIDDEN_CLASSNAME);
-    loginTodo.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
@@ -35,11 +36,11 @@ const savedUsername = localStorage.getItem(USERNAME_KEY);
 if (savedUsername === null) {
     loginForm.classList.remove(HIDDEN_CLASSNAME);
     alertLogin.classList.remove(HIDDEN_CLASSNAME);
+    sidebarNav.classList.add(NONE_CLICK)
     logoutForm.classList.add(HIDDEN_CLASSNAME);
-    loginTodo.classList.add(HIDDEN_CLASSNAME);
     loginForm.addEventListener("submit", onLoginSubmit);
 } else {
     paintGreetings(savedUsername);
 }
 
-logoutForm.addEventListener("click", onLogoutClick);
+sidebarLogout.addEventListener("click", onLogoutClick);
--- js/sidebar.js
+++ js/sidebar.js
@@ -1,7 +1,7 @@
 const body = document.querySelector("body"),
 sidebar = body.querySelector(".sidebar"),
 toggle = body.querySelector(".toggle"),
-searchBtn = body.querySelector(".search-box"),
+searchBtn = body.querySelector(".write-box"),
 modeSwitch = body.querySelector(".toggle-switch"),
 modeText = body.querySelector(".mode-text"),
 mainHome = body.querySelector(".home");
